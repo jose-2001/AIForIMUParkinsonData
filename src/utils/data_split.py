850b62e022514e4e0aec71a9c01ea9629fc124a3
Julian Bolaños <julian.camilob24@gmail.com>
2024-03-19 14:39:02 -0500

    fix: CSV writing processed fixed.

--- src/utils/data_split.py
+++ src/utils/data_split.py
@@ -1,6 +1,7 @@
 import pickle
 
 from pandas import DataFrame
+import numpy as np
 from sklearn.model_selection import train_test_split
 from src.settings import ROOT_DIR
 
@@ -10,6 +11,8 @@ TEST_PERCENTAGE = 0.1
 
 SEED = 11
 
+PROCESSED_PATH = ROOT_DIR / 'data' / 'processed'
+
 
 def save_and_split(df: DataFrame, module: str):
     train, test = train_test_split(df, test_size=1 - TRAIN_PERCENTAGE, random_state=SEED)
@@ -18,18 +21,17 @@ def save_and_split(df: DataFrame, module: str):
 
     filename = module + '/train'
     write_pickle(filename + '.pkl', train)
-    write_pickle(filename + '.csv', train)
+    DataFrame(train).to_csv(PROCESSED_PATH / filename + '.csv')
 
     filename = module + '/test'
     write_pickle(filename + '.pkl', test)
-    write_pickle(filename + '.csv', test)
+    DataFrame(test).to_csv(PROCESSED_PATH / filename + '.csv')
 
     filename = module + '/val'
     write_pickle(filename + '.pkl', val)
-    write_pickle(filename + '.csv', val)
+    DataFrame(val).to_csv(PROCESSED_PATH / filename + '.csv')
 
 
-def write_pickle(filename: str, variable: any) -> None:
-    processed_path = ROOT_DIR / 'data' / 'processed'
-    with open(processed_path / filename, 'wb') as file:
+def write_pickle(filename: str, variable: np.array) -> None:
+    with open(PROCESSED_PATH / filename, 'x') as file:
         pickle.dump(variable, file)
