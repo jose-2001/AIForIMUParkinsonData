a57cc5936afc682eb548673652aacce6300eb778
Julian Bolaños <julian.camilob24@gmail.com>
2024-03-25 23:24:42 -0500

    feat: Defined 4 LSTM modules to test.

--- src/model/asa_models.py
+++ src/model/asa_models.py
@@ -1,20 +1,65 @@
-import tensorflow
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout, InputLayer
+from tensorflow.keras.layers import LSTM, Dense, InputLayer, Conv1D, Dropout
 
 DEFAULT_OPTIMIZER = 'adam'
 DEFAULT_LEARNING_RATE = 0.001
-DEFAULT_NUM_EPOCHS = 10
-DEFAULT_LOSS_FUNCTION = 'mse'
+DEFAULT_LOSS_FUNCTION = 'binary_crossentropy'
 DEFAULT_ACCURACY_FUNCTION = 'sigmoid'
 
+INPUT_SHAPE = (16723, 24)
 
-def build_basic_lstm(neurons: int) -> Sequential:
+
+def build_basic_lstm() -> Sequential:
+    model = Sequential([
+        LSTM(64, input_shape=INPUT_SHAPE),
+        Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(loss=DEFAULT_LOSS_FUNCTION,
+                  optimizer=DEFAULT_OPTIMIZER,
+                  metrics=['accuracy'])
+
+    return model
+
+
+def build_2layer_lstm() -> Sequential:
     model = Sequential()
-    model.add(InputLayer(input_shape=(None, 24)))
-    model.add(LSTM(neurons, input_shape=(24,), return_sequences=True))
-    model.add(LSTM(neurons, return_sequences=True))
-    model.add(Dense(units=1))
+    model.add(InputLayer(input_shape=INPUT_SHAPE))
+    model.add(LSTM(128, return_sequences=True))
+    model.add(LSTM(128, return_sequences=True))
+    model.add(Dense(units=1, activation='sigmoid'))
+
+    model.compile(loss=DEFAULT_LOSS_FUNCTION,
+                  optimizer=DEFAULT_OPTIMIZER,
+                  metrics=['accuracy'])
+
+    return model
+
+
+def build_1d_conv_2layer_lstm() -> Sequential:
+    model = Sequential()
+    model.add(InputLayer(input_shape=INPUT_SHAPE))
+    model.add()
+    model.add(LSTM(128, return_sequences=True))
+    model.add(LSTM(128, return_sequences=True))
+    model.add(Dense(units=1, activation='sigmoid'))
+
+    model.compile(loss=DEFAULT_LOSS_FUNCTION,
+                  optimizer=DEFAULT_OPTIMIZER,
+                  metrics=['accuracy'])
+
+    return model
+
+
+def build_1d_conv_2layer_lstm_do() -> Sequential:
+    model = Sequential()
+    model.add(InputLayer(input_shape=INPUT_SHAPE))
+    model.add()
+    model.add(LSTM(128, return_sequences=True))
+    model.add(Dropout(rate=0.2))
+    model.add(LSTM(256, return_sequences=True))
+    model.add(Dropout(rate=0.2))
+    model.add(Dense(units=1, activation='sigmoid'))
 
     model.compile(loss=DEFAULT_LOSS_FUNCTION,
                   optimizer=DEFAULT_OPTIMIZER,
