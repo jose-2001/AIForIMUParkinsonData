19cf545b6877fa43506aecdc1c6f69e71a1a8d70
Julian Bolaños <julian.camilob24@gmail.com>
2024-03-28 18:28:21 -0500

    feat: Random Search implemented on simple lstm model.

--- src/model/asa_models.py
+++ src/model/asa_models.py
@@ -1,5 +1,6 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, InputLayer, Conv1D, Dropout
+from tensorflow.keras.optimizers import Adam
 
 DEFAULT_OPTIMIZER = 'adam'
 DEFAULT_LEARNING_RATE = 0.001
@@ -9,14 +10,17 @@ DEFAULT_ACCURACY_FUNCTION = 'sigmoid'
 INPUT_SHAPE = (16723, 24)
 
 
-def build_basic_lstm() -> Sequential:
+def build_basic_lstm(hp) -> Sequential:
+    hp_units = hp.Int('units_hp', min_value=32, max_value=128, step=32)
     model = Sequential([
-        LSTM(64, input_shape=INPUT_SHAPE),
+        LSTM(hp_units, input_shape=INPUT_SHAPE),
         Dense(1, activation='sigmoid')
     ])
 
+    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3,1e-4])
+    opt = Adam(learning_rate=hp_learning_rate)
     model.compile(loss=DEFAULT_LOSS_FUNCTION,
-                  optimizer=DEFAULT_OPTIMIZER,
+                  optimizer=opt,
                   metrics=['accuracy'])
 
     return model
@@ -39,7 +43,7 @@ def build_2layer_lstm() -> Sequential:
 def build_1d_conv_2layer_lstm() -> Sequential:
     model = Sequential()
     model.add(InputLayer(input_shape=INPUT_SHAPE))
-    model.add()
+    model.add(Conv1D(filters=32, kernel_size=3, strides=1, padding='causal', activation='relu'))
     model.add(LSTM(128, return_sequences=True))
     model.add(LSTM(128, return_sequences=True))
     model.add(Dense(units=1, activation='sigmoid'))
