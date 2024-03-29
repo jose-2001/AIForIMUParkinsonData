54918e26deed0c30228813ad115250f5ef7930b3
Julian Bolaños <julian.camilob24@gmail.com>
2024-03-28 22:47:22 -0500

    feat: Added hyperparameter options to model creation models.

--- src/model/asa_models.py
+++ src/model/asa_models.py
@@ -3,9 +3,9 @@ from tensorflow.keras.layers import LSTM, Dense, InputLayer, Conv1D, Dropout
 from tensorflow.keras.optimizers import Adam
 
 DEFAULT_OPTIMIZER = 'adam'
-DEFAULT_LEARNING_RATE = 0.001
+DEFAULT_LEARNING_RATE = 1e-2
 DEFAULT_LOSS_FUNCTION = 'binary_crossentropy'
-DEFAULT_ACCURACY_FUNCTION = 'sigmoid'
+DEFAULT_ACTIVATION_FUNCTION = 'sigmoid'
 
 INPUT_SHAPE = (16723, 24)
 
@@ -26,47 +26,69 @@ def build_basic_lstm(hp) -> Sequential:
     return model
 
 
-def build_2layer_lstm() -> Sequential:
+def build_2layer_lstm(hp) -> Sequential:
     model = Sequential()
     model.add(InputLayer(input_shape=INPUT_SHAPE))
     model.add(LSTM(128, return_sequences=True))
-    model.add(LSTM(128, return_sequences=True))
-    model.add(Dense(units=1, activation='sigmoid'))
+    hp_units = hp.Int('units_hp', min_value=32, max_value=256, step=32)
+    model.add(LSTM(hp_units, return_sequences=True))
+    hp_dense_units = hp.Int('dense_units_hp', min_value=32, max_value=128, step=32)
+    model.add(Dense(units=hp_dense_units, activation='relu'))
+    model.add(Dense(units=1, activation=DEFAULT_ACTIVATION_FUNCTION))
+
+    opt = Adam(learning_rate=DEFAULT_LEARNING_RATE)
+    model.compile(loss=DEFAULT_LOSS_FUNCTION,
+                  optimizer=opt,
+                  metrics=['accuracy'])
 
     model.compile(loss=DEFAULT_LOSS_FUNCTION,
-                  optimizer=DEFAULT_OPTIMIZER,
+                  optimizer=opt,
                   metrics=['accuracy'])
 
     return model
 
 
-def build_1d_conv_2layer_lstm() -> Sequential:
+def build_1d_conv_2layer_lstm(hp) -> Sequential:
     model = Sequential()
     model.add(InputLayer(input_shape=INPUT_SHAPE))
-    model.add(Conv1D(filters=32, kernel_size=3, strides=1, padding='causal', activation='relu'))
+
+    hp_filters = hp.Int('filters_hp', min_value=5, max_value=32)
+    hp_kernel = hp.Int('kernel_hp', min_value=8, max_value=64, step=8)
+    model.add(Conv1D(filters=hp_filters, kernel_size=hp_kernel, strides=1, padding='causal', activation='relu'))
     model.add(LSTM(128, return_sequences=True))
     model.add(LSTM(128, return_sequences=True))
     model.add(Dense(units=1, activation='sigmoid'))
 
+    opt = Adam(learning_rate=DEFAULT_LEARNING_RATE)
     model.compile(loss=DEFAULT_LOSS_FUNCTION,
-                  optimizer=DEFAULT_OPTIMIZER,
+                  optimizer=opt,
+                  metrics=['accuracy'])
+
+    model.compile(loss=DEFAULT_LOSS_FUNCTION,
+                  optimizer=opt,
                   metrics=['accuracy'])
 
     return model
 
 
-def build_1d_conv_2layer_lstm_do() -> Sequential:
+def build_1d_conv_2layer_lstm_do(hp) -> Sequential:
     model = Sequential()
     model.add(InputLayer(input_shape=INPUT_SHAPE))
-    model.add()
+    # TODO: Add neurons according to results of previous models
+    model.add(model.add(Conv1D(filters=0, kernel_size=0, strides=1, padding='causal', activation='relu')))
     model.add(LSTM(128, return_sequences=True))
     model.add(Dropout(rate=0.2))
     model.add(LSTM(256, return_sequences=True))
     model.add(Dropout(rate=0.2))
     model.add(Dense(units=1, activation='sigmoid'))
 
+    opt = Adam(learning_rate=DEFAULT_LEARNING_RATE)
     model.compile(loss=DEFAULT_LOSS_FUNCTION,
-                  optimizer=DEFAULT_OPTIMIZER,
+                  optimizer=opt,
+                  metrics=['accuracy'])
+
+    model.compile(loss=DEFAULT_LOSS_FUNCTION,
+                  optimizer=opt,
                   metrics=['accuracy'])
 
     return model
